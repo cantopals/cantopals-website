98eb13e52bf7b02313fc1a31f1370024677cbbb9
Ismail Kattakath <ikattakath@homewoodhealth.com>
2023-01-25 06:13:05 -0500

    Modified common header

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className="uk-background-white dark:uk-background-gray-100 dark:uk-text-gray-20">
       <Head>
-          <title>Nerko - NFT Portfolio Landing Template</title>
+          <title>CantoPals - Premium NFTs on Canto</title>
         </Head>
       <body className="uni-body">
         <Main />
