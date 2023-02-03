0453d6b2749c5b7076cca45918d04dc6134bf2fc
Ismail Kattakath <ikattakath@homewoodhealth.com>
2023-02-03 10:18:58 -0500

    Changed logo

--- components/common/Logo.js
+++ components/common/Logo.js
@@ -1,8 +1,8 @@
 const Logo = () => {
     return (
         <>
-            <img className="uk-visible dark:uk-hidden" width="120" src="/images/canto-logo.png" alt="Nerko" loading="lazy" />
-            <img className="uk-hidden dark:uk-visible" width="120" src="/images/canto-logo.png" alt="Nerko" loading="lazy" />
+            <img className="uk-visible dark:uk-hidden" width="120" src="/images/canto-pals-dark.svg" alt="Nerko" loading="lazy" />
+            <img className="uk-hidden dark:uk-visible" width="120" src="/images/canto-pals-dark.svg" alt="Nerko" loading="lazy" />
         </>
     )
 }
