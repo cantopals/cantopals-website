766480ead8e92184f99e89d1ce87c2358a90f93d
Canto Pal <cantopals@proton.me>
2023-02-05 01:46:57 -0500

    switch network and totalsupply implemented

--- components/cta/HomeOneCta.js
+++ components/cta/HomeOneCta.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-import { useMoralis, useWeb3Contract } from 'react-moralis';
+import { useMoralis, useWeb3Contract, useChain } from 'react-moralis';
 import { abi } from '../../constants/abi';
 import { contractAddress } from '../../constants/contract';
 import { useEffect } from 'react';
@@ -10,11 +10,16 @@ import { whitelistedAddresses } from '../../constants/whitelist';
 
 const HomeOneCta = () => {
 
-    const { enableWeb3, isWeb3Enabled, account } = useMoralis();
+    const { enableWeb3, isWeb3Enabled, account, chainId } = useMoralis();
+    const { switchNetwork } = useChain();
     const { data, error, runContractFunction, isFetching, isLoading } = useWeb3Contract();
     let [mintAmount, setMintAmount] = useState(1);
     let [claimable, setClaimable] = useState(false);
-    const mintFee = 12;
+    let [totalSupply, setTotalSupply] = useState(0);
+    const mainnetChainId = '0x1e14';
+    const testnetChainId = '0x2e4'
+    const mintFee = chainId === testnetChainId ? 1 : 12; // If not Testnet, 12
+    const maxSupply = 3333;
 
     useEffect(() => {
         if (account) {
@@ -52,17 +57,25 @@ const HomeOneCta = () => {
         functionName: 'claim'
     };
 
-    /* useEffect(() => {
-        if (error) {
-            console.log({ error });
-        }
-    }, [error]);
+    const totalSupplyOptions = {
+        abi,
+        contractAddress,
+        functionName: 'totalSupply'
+    }
 
     useEffect(() => {
-        if (data) {
-            console.log({ data });
+        if (chainId && chainId !== mainnetChainId) { //Should be 0x1e14, hex of 7700
+            // console.log('CHAINID: ', chainId);
+            switchNetwork(mainnetChainId)
         }
-    }) */
+    }, [chainId]);
+
+    useEffect(() => {
+        runContractFunction({ params: totalSupplyOptions })
+            .then((result) => {
+                setTotalSupply(parseInt(result?._hex, 16))
+            })
+    }, [isWeb3Enabled])
 
     return (
         <>
@@ -79,6 +92,7 @@ const HomeOneCta = () => {
                                         <button onClick={() => incrementMintAmount()} className="uk-button uk-button-small uk-button-large@m uk-button-gradient uk-margin-small-top uk-margin-large-top@m">+</button>
                                     </div>
                                     <button onClick={() => runContractFunction({ params: mintOptions })} disabled={isFetching} className="uk-button uk-button-small uk-button-large@m uk-button-gradient uk-margin-small-top uk-margin-large-top@m mintButton">Mint</button>
+                                    <p>{`${totalSupply} / ${maxSupply} NFTs have been minted so far.`}</p>
                                     {claimable &&
                                         <button onClick={() => runContractFunction({ params: claimOptions })} disabled={isFetching} className="uk-button uk-button-small uk-button-large@m uk-button-gradient uk-margin-small-top uk-margin-large-top@m">Claim</button>
                                     }
@@ -98,12 +112,23 @@ const HomeOneCta = () => {
                             </ul>
                         </div>
                     }
-                    {data &&
+                    {data && data?.hash && !isLoading &&
                         <div className="uk-panel uk-margin-medium-top uk-margin-2xlarge-top@m">
                             <ul className="uk-card uk-card-small uk-card-large@m uk-radius uk-radius-large@m uk-width-2xlarge@m uk-margin-auto uk-box-shadow-xsmall dark:uk-background-white-5" data-uk-accordion="collapsible: false" data-anime="opacity:[0, 1]; translateY:[24, 0]; onview: true; delay: 100;">
                                 <li>
                                     <div className="uk-accordion-content uk-padding-small-bottom">
-                                        <p className="uk-text-small uk-text-large@m uk-text-muted">Transaction Successful. <Link href={`https://evm.explorer.canto.io/tx/${data?.hash}`} target={'_blank'}>Check Block Explorer!</Link> (It may take a few minutes for the Transaction to get confirm though.)</p>
+                                        <p className="uk-text-small uk-text-large@m uk-text-muted">Transaction Successful. <Link href={`https://${chainId === testnetChainId ? 'testnet-explorer.canto.neobase.one' : 'evm.explorer.canto.io'}/tx/${data?.hash}`} target={'_blank'}>Check Block Explorer.</Link> (It may take a few minutes for the Transaction to get confirm though.)</p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    }
+                    {isLoading &&
+                        <div className="uk-panel uk-margin-medium-top uk-margin-2xlarge-top@m">
+                            <ul className="uk-card uk-card-small uk-card-large@m uk-radius uk-radius-large@m uk-width-2xlarge@m uk-margin-auto uk-box-shadow-xsmall dark:uk-background-white-5" data-uk-accordion="collapsible: false" data-anime="opacity:[0, 1]; translateY:[24, 0]; onview: true; delay: 100;">
+                                <li>
+                                    <div className="uk-accordion-content uk-padding-small-bottom">
+                                        <p className="uk-text-small uk-text-large@m uk-text-muted">Your Transaction is being processed. Please wait!</p>
                                     </div>
                                 </li>
                             </ul>
